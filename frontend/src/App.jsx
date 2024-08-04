96ce625f289d2ec19cf1a34c31c21c0350f6435c
MAYANK PATHAK <mayankpathak526@gmail.com>
2024-08-04 09:35:39 +0530

    Fix environment variable issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import Home from './components/Home'
-import NotFound from './components/Notfound';
+import Home from './components/Home';
+import NotFound from './components/Notfound'; // Ensure this file exists
+
 function App() {
   return (
-    <div className='app'>
-      <BrowserRouter>
-       
-        <Routes>
-          <Route path="/" element={<Login />} /> 
-          <Route path="/signup" element={<Signup />} /> 
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/home" element={< Home/>} /> 
-          <Route path="*" element={<NotFound/>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
